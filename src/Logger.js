69e0b4e4fbabadff67eddd05bb4685e26c269033
Alexander Sims <alexander.sims92@gmail.com>
2016-08-21 15:09:06 +0100

    Logger now appends date

--- src/Logger.js
+++ src/Logger.js
@@ -3,13 +3,15 @@ var fs = require('fs');
 var Logger = function() {};
 
 Logger.log = function(filename, data) {
-    fs.writeFile(filename, data, function(err) {
+    var message = '[' + new Date() + ']' + data;
+    fs.writeFile(filename, message, function(err) {
         if(err) console.log('caller: ' + data + ', ERROR: ' + JSON.stringify(err));
     });
 };
 
 Logger.append = function(filename, data) {
-    fs.appendFile(filename, ('\n' + data), function(err) {
+    var message = '[' + new Date() + ']' + data;
+    fs.appendFile(filename, ('\n' + message), function(err) {
         if(err) console.log('caller: ' + data + ', ERROR: ' + JSON.stringify(err));
     });
 };
