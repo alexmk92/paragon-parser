dd9827d69e6f963890e34bec4d0e9a12cf72d2ac
Alexander Sims <alexander.sims92@gmail.com>
2016-08-21 18:00:48 +0100

    Try this

--- parser.js
+++ parser.js
@@ -9,6 +9,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 var url = 'mongodb://' + config.MONGO_HOST + '/' + config.MONGO_DATABASE;
 var mongodb = null;
+var queue   = null;
 var workers = 1;
 
 // Take command line arguments
@@ -23,23 +24,54 @@ process.argv.some(function (val, index) {
     return false;
 });
 
-//catches uncaught exceptions
-process.on('uncaughtException', function (err) {
-    console.error((new Date).toUTCString() + ' uncaughtException:', err.message);
-    console.error(err.stack);
-    process.exit(1);
-});
-
-// Actual process code here
 MongoClient.connect(url, function(err, db) {
     mongodb = db;
     if(cluster.isMaster) {
-        // do the initial fork
+
         cluster.fork();
 
         cluster.on('exit', function(worker) {
             console.log('[PARSER] Process %s died. Restarting...', worker.process.pid);
             cluster.fork();
         });
+    }
+
+    if(cluster.isWorker) {
+        var cleaningUp = false;
+
+        if(!queue) queue = new Queue(mongodb, workers);
+
+        // Handle closing here:
+        //process.stdin.resume();//so the program will not close instantly
+
+        // function cleanup() {
+        //     if(queue) { queue.killConnections(); }
+        //     if (!cleaningUp) {
+        //         cleaningUp = true;
+        //         /*
+        //         queue.stop(function() {
+        //             // clean up any processes which were put on queue afterward (need to look at this but
+        //             // its a temp plaster for now :))
+        //             queue.stop(function() {
+        //                 cleaningUp = false;
+        //                 console.log('[PARSER] All workers were shut down successfully'.yellow);
+        //                 process.exit();
+        //             });
+        //         });
+        //         */
+        //     }
+        // }
+        //
+        // //do something when app is closing
+        // process.on('exit', cleanup);
+        //
+        // //catches ctrl+c event
+        // process.on('SIGINT', cleanup);
+        //
+        // //catches uncaught exceptions
+        // process.on('uncaughtException', function(err) {
+        //     console.log('[PARSER] Uncaught Exception: '.red, err);
+        //     cleanup();
+        // });
     }
 });
