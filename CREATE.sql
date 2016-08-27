c7edc539d912326271592beb2f1f2390eeecc575
Alexander Sims <alexander.sims92@gmail.com>
2016-08-27 12:18:33 +0100

    Removed reserved column from the SQL seed table

--- CREATE.sql
+++ CREATE.sql
@@ -2,15 +2,14 @@ CREATE TABLE queue (
     id int not null auto_increment,
     replayId varchar(50) not null,
     attempts int not null DEFAULT 0,
-    reserved bool not null DEFAULT false,
     reserved_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    reserved_by VARCHAR(24) DEFAULT null,
     completed bool not null DEFAULT false,
     priority int not null DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     completed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     scheduled TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     checkpointTime int DEFAULT 0,
-    reserved_by VARCHAR(24) DEFAULT null,
     live bool not null DEFAULT false,
     primary key (id)
 );
