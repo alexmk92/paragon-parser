7b9e1b2c041c3b27c48dfd82a5ab6f49eee72236
Alexander Sims <alexander.sims92@gmail.com>
2016-08-27 15:25:15 +0100

    Added an SQL Port .env variable

--- src/Connection.js
+++ src/Connection.js
@@ -9,7 +9,7 @@ var Connection = function() {
         user:     process.env.SQL_USER,
         password: process.env.SQL_PASSWORD,
         database: process.env.SQL_DATABASE,
-        port:     3306
+        port:     process.env.SQL_PORT
     });
 };
 
