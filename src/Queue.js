3ac8b5138810adb457cb18837e320cd9b292eab9
Alexander Sims <alexander.sims92@gmail.com>
2016-08-29 19:42:37 +0100

    Removing true now never happens

--- src/Queue.js
+++ src/Queue.js
@@ -9,7 +9,7 @@ var Memcached = require('memcached');
 var memcached = new Memcached(process.env.MEMCACHED_HOST + ':' + process.env.MEMCACHED_PORT);
 
 // Local lock to determine if we are removing from workers array
-var removing = false;
+//var removing = false;
 
 /**
  * @Queue :
@@ -167,7 +167,7 @@ Queue.prototype.runTask = function(replay) {
 Queue.prototype.workerDone = function(replay) {
     return new Promise(function(resolve, reject) {
         if(!removing) {
-            removing = true;
+            //removing = true;
             var index = -1;
             this.workers.some(function(workerId, i) {
                 if(workerId === replay.replayId) {
